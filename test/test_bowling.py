611b98854f927c107d16777aa17a380a30b5b475
Heikki H <heikki.haapaniemi.gml@gmail.com>
2024-10-28 16:48:31 +0200

    US1 - test 1

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -5,7 +5,15 @@ from bowling_error import BowlingError
 from frame import Frame
 
 
+
 class TestBowlingGame(unittest.TestCase):
 
-    def test_something(self):
-        pass
+    def test_game_created(self):
+        f = Frame(1 , 5)
+        game = BowlingGame()
+        game.add_frame(f)
+        self.assertEqual(f, game.get_frame_at(0) )
+
+
+
+
